0173443c2aacc008219eb602197dc22fed28a925
Santhosh-Nagarajan <103426295+Santhosh-Nagarajan@users.noreply.github.com>
2023-08-03 21:07:22 +0530

    Update main.tf

--- modules/register/main.tf
+++ modules/register/main.tf
@@ -3,7 +3,7 @@ resource "docker_image" "zoo" {
   name = "web"
   build {
     context = "."
-    tag     = ["santhoshsp/login:v1"]
+    tag     = ["docker.io/santhoshsp/login:v1"]
     label = {
       author : "santhosh"
     }
@@ -13,7 +13,7 @@ resource "docker_image" "zoo" {
 
 resource "null_resource" "demo" {
   provisioner "local-exec" {
-    command = " sudo docker login -u santhoshsp -p san@12345 && docker push santhoshsp/login:v1"
+    command = " sudo docker login -u santhoshsp -p san@12345 && docker push docker.io/santhoshsp/login:v1"
   }
 }
 
